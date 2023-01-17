314e2adac972f9fd33d110f0f16b22e5f5d4620d
min-kim123 <8450minkim@gmail.com>
2023-01-17 01:15:33 -0800

    need to fix deleet and do sort. finished average

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
+#include <iomanip>//include for setprecision thing
 #include "student.h"
 #include "node.h"
 using namespace std;
-
 /*
 Author: Min Kim
 Program Description: This is a student list program utilizing a linked list. 
@@ -10,36 +10,53 @@ You can add, print, and delete students, and also print the averages of all the 
 Date: 1/27/23
 */
 
-void add(Node* head, Node* newnode) {//insert and sort
-    if (head->next != NULL) {
-        cout << "inside add function, head is not null";
-        head = head->next;
-        cout << head->student->id;//test to see if it moves through all previous inputs
+void add(Node* head, Node* newnode) {//insert done, working on sort
+    if (head->getNext() != NULL) {
+        head = head->getNext();
+        //cout << head->getStudent()->id;//test to see if it moves through all previous inputs
         add(head, newnode);//recursion, move through all existing nodes
     }
     else {//only accessed after recursion
-        head->next = newnode;
+        head->next = newnode;//for some reason this only works if it's "next" and not "getNext()"
     }
-    
-        //head->next = newnode;//after traveling through all nodes, set next node to newnode*/
-    
 }
 
-void print(Node* n) {
+void print(Node* n) {//done
+    cout << fixed << setprecision(2);//for gpa to have 2 decimal places
     if (n != NULL) {
         cout << n->getStudent()->id;
         n = n->getNext();
         print(n);//recursion
     }
 }
-/*
-void delet() {
 
+void deleet(Node* head, int id) {//doesn't work if it's the first one.
+//make a temp pointer
+    if (id == head->getStudent()->id) {//for the first one- just delete it no reconnecting
+    }
+    else if (id == head->getNext()->getStudent()->id) {//if NEXT node is equal
+        //delete head;//pointer being freed was not allocated
+        head->next = head->getNext()->getNext();//reconnecting nodes after deletion
+    }
+    else {
+        head = head->getNext();
+        deleet(head, id);
+    }
 }
-void average() {
 
+void average(Node* head, float total, int count) {//done
+    cout << fixed << setprecision(2);//for gpa to have 2 decimal places
+    if (head->getNext() != NULL) {
+        head = head->getNext();
+        total = total + head->getStudent()->id;//CHANGE TO GPA
+        count++;
+        average(head, total, count);
+    }
+    else {
+        cout << "Average GPA: " << total/count << endl;
+    }
 }
-*/
+
 int main(){
 
     Node* head = NULL;
@@ -48,7 +65,7 @@ int main(){
     bool cont = true;
 
     while (cont == true) {
-        cout << "Add, print, or quit? (add/print/quit): ";
+        cout << "Add, print, delete, average, or quit? (add/print/delete/average/quit): ";
         cin >> input;
         cin.ignore();
         if (strcmp(input, "add") == 0) {//user inputs "add"
@@ -62,7 +79,7 @@ int main(){
             if (head == NULL) {
                 cout << "head is null";
                 head = newnode;
-                cout << head->student->id;
+                cout << head->getStudent()->id;
             }
             else {
                 cout << "head is not null" << endl;
@@ -72,9 +89,21 @@ int main(){
         else if (strcmp(input, "print") == 0) {//user inputs "print"
             print(head);
         }
-        /*else if (strcmp(input, "delete") == 0) {//user inputs "delete"
-            delet();
-        }*/
+        else if (strcmp(input, "delete") == 0) {//user inputs "delete"
+            cout << "ID: ";
+            cin >> id;
+            if (head == NULL) {
+                cout << "Empty list. Nothing to delete" << endl;
+            }
+            else {
+                deleet(head, id);
+            }
+        }
+        else if (strcmp(input, "average") == 0) {//user inputs "average"
+            float total = head->getStudent()->id;
+            int count = 1;
+            average(head, total, count);
+        }
         else if (strcmp(input, "quit") == 0) {//user inputs "quit"
             cont = false;
         }
